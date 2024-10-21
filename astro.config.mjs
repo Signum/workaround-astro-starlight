2fdf2840cbc9bcdc317a0dc89c6c9c4371bead0f
Christoph Haas <email@christoph-haas.de>
2024-10-21 23:37:58 +0200

    Customized CSS, footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,36 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://workaround.org',
-	integrations: [
-		starlight({
-			title: 'ISPmail Guide',
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Start Here', slug: 'ispmail-bookworm' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			logo: {
-				light: './src/assets/logo.svg',
-    		dark: './src/assets/logo-dark.svg',
-    		replacesTitle: true,
-			},
-		}),
-		sitemap()
-	],
+  site: "https://workaround.org",
+  integrations: [
+    starlight({
+      lastUpdated: true,
+      title: "ISPmail Guide",
+      components: {
+        Footer: "./src/components/Footer.astro",
+      },
+      customCss: [
+        "./src/styles/custom.css",
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Start Here", slug: "ispmail-bookworm" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      logo: {
+        light: "./src/assets/logo.svg",
+        dark: "./src/assets/logo-dark.svg",
+        replacesTitle: true,
+      },
+    }),
+    sitemap(),
+  ],
 });
